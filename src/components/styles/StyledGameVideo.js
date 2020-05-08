06b8df7e6f36a5af1ee3915236ffb7428c756e4a
Michael De La Rosa <56710845+m-delarosa@users.noreply.github.com>
2020-05-08 11:45:00 -0600

    Add Game.js carousel component.

--- src/components/styles/StyledGameVideo.js
+++ src/components/styles/StyledGameVideo.js
@@ -2,11 +2,23 @@ import styled from 'styled-components'
 
 export const StyledGameVideo = styled.div`
   background-color: #353535;
-  display: flex;
-  justify-content: center;
+  width: 100%;
+  height: 75%;
+//   display: flex;
+//   justify-content: center;
 
   video {
       margin-bottom: 22px;
       cursor: pointer;
+      width: 75%;
+      height: 75%;
+      position: relative;
+      right: -200px;
+      top: 120px;
+  }
+
+  img {
+      width: 100%;
+      height: 100%;
   }
 `
