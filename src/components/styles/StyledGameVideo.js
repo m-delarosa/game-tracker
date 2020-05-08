7c1f2575d7bf7610597cfad1e52e4defb5872e5b
github username <github email address>
2020-05-07 23:13:15 -0600

    Add video element to Game component

---
+++ src/components/styles/StyledGameVideo.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const StyledGameVideo = styled.div`
+  background-color: #353535;
+  display: flex;
+  justify-content: center;
+
+  video {
+      margin-bottom: 22px;
+      cursor: pointer;
+  }
+`
